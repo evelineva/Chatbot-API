99ffacebefdceb8d2f6e4af4ac65382c3e18e202
evelineva <eveline.anthony@student.matanauniversity.ac.id>
2025-05-24 20:34:49 +0700

    Update HDActions.js

--- routes/HDActions.js
+++ routes/HDActions.js
@@ -7,7 +7,7 @@ const generateCustomId = require("../utils/generateCustomId");
 
 router.get("/", verifyToken, async (req, res) => {
   try {
-    const userId = req.userId; 
+    const userId = req.user.userId; 
     const data = await HdAction.find({ userId }).sort({ createdAt: -1 }); 
     res.json(data);
   } catch (err) {
