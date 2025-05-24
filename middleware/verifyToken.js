c4e04f75403f1264d59befb233578df84fa735b0
evelineva <eveline.anthony@student.matanauniversity.ac.id>
2025-05-24 20:12:13 +0700

    Update verifyToken.js

--- middleware/verifyToken.js
+++ middleware/verifyToken.js
@@ -11,7 +11,7 @@ const verifyToken = (req, res, next) => {
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded;
+    req.userId = decoded.id;
     next();
   } catch (err) {
     res.status(401).json({ message: "Invalid token." });
